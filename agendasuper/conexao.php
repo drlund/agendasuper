0ca165720c6fc83b305866402f26644d8a4a4305
Paulo César Souza Pereira <drlund@hotmail.com>
2021-03-31 19:24:32 -0300

    Delete agendasuper directory

--- agendasuper/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "agenda";
-$username = "root";
-$password = "";
-
-//Criar conexão:
-$conexao = mysqli_connect($servername, $username, $password, $database);
